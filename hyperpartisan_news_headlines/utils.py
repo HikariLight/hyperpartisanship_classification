c5dc2c8777e825fc11eeec03a722db2a8b31dfc8
Dhia Eddine Merzougui <dhia-eddine.merzougui@etu.unicaen.fr>
2024-12-18 09:52:50 +0100

    Added Vista (HP news headlines) dataset scripts

--- hyperpartisan_news_headlines/utils.py
+++ hyperpartisan_news_headlines/utils.py
@@ -1,25 +1,5 @@
 import numpy as np
-
-
-def compute_average_metrics(data):
-    metrics = ["eval_accuracy", "eval_recall", "eval_precision", "eval_f1-score"]
-    result = {}
-
-    for metric in metrics:
-        scores = [element[metric] for element in data]
-        avg = np.mean(scores)
-        minimum = np.min(scores)
-        maximum = np.max(scores)
-        stddev = np.std(scores)
-
-        result[metric] = {
-            "score": avg,
-            "stddev": stddev,
-            "min": minimum,
-            "max": maximum,
-        }
-
-    return result
+import evaluate
 
 
 def get_dataset_length_stats(tokenizer, dataset):
@@ -40,3 +20,67 @@ def get_dataset_length_stats(tokenizer, dataset):
         }
 
     return result
+
+
+def compute_metrics(predictions, labels):
+    accuracy_metric = evaluate.load("accuracy")
+    precision_metric = evaluate.load("precision")
+    recall_metric = evaluate.load("recall")
+    f1_metric = evaluate.load("f1")
+
+    accuracy = accuracy_metric.compute(predictions=predictions, references=labels)[
+        "accuracy"
+    ]
+    precision = precision_metric.compute(
+        predictions=predictions, references=labels, average="weighted"
+    )["precision"]
+    recall = recall_metric.compute(
+        predictions=predictions, references=labels, average="weighted"
+    )["recall"]
+    f1 = f1_metric.compute(
+        predictions=predictions, references=labels, average="weighted"
+    )["f1"]
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+    }
+
+
+def compute_average_metrics(data):
+    result = {}
+    metrics = data[0].keys()
+
+    for metric in metrics:
+        scores = [element[metric] for element in data]
+        avg = np.mean(scores)
+        minimum = np.min(scores)
+        maximum = np.max(scores)
+        stddev = np.std(scores)
+
+        result[metric] = {
+            "score": avg,
+            "stddev": stddev,
+            "min": float(minimum),
+            "max": float(maximum),
+        }
+
+    return result
+
+
+def compute_fews_hot_nested_avg(nested_evals):
+    avg_evals = {}
+    shots = list(nested_evals[list(nested_evals.keys())[0]].keys())
+
+    for shot in shots:
+        aggregate_evals = []
+
+        for nested_result in nested_evals:
+            aggregate_evals.append(nested_evals[nested_result][shot])
+
+        evals = compute_average_metrics(aggregate_evals)
+        avg_evals[shot] = evals
+
+    return avg_evals
